8e801bb5f7602016ba24fb5603bd2725276fb12d
Jason Phan <jase.phan@mail.utoronto.ca>
2025-11-10 00:38:07 -0500

    Hero

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,23 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
+import './index.css';
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero'; 
+
 
 function App() {
-
   return (
-    <>
-      <Header />
 
-    </>
-  )
+    <div className="bg-[#fffaf3] min-h-screen text-[#575279]">
+      <Header />
+      <Hero />
+      <main>
+        {/* other sections */}
+      </main>
+    </div>
+  );
 }
 
 export default App;
+
