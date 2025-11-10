47c6131a3eb9b8f3b04d74a1b1a5b17a00e1e98d
Jason Phan <jase.phan@mail.utoronto.ca>
2025-11-10 00:52:42 -0500

    dockerized

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+//import { useState } from 'react';
+//import reactLogo from './assets/react.svg';
+//import viteLogo from '/vite.svg';
 import './index.css';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero'; 
