5fb922326a96244b9f4e16a92b74e316aaa92497
Jason Phan <jase.phan@mail.utoronto.ca>
2025-11-13 18:54:46 -0500

    project commit board

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from app.routes.contributions import bp as contributions_bp
+from flask_cors import CORS
+from routes.contributions import bp as contributions_bp
 
 if __name__ == '__main__': 
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(contributions_bp)
     app.run(debug=True) 
 
--- backend/app/routes/contributions.py
+++ backend/app/routes/contributions.py
@@ -1,9 +1,8 @@
 from flask import Blueprint
-from app.services.contributions_service import contributions_service
+from services.contributions_service import contributions_service
 
 bp = Blueprint("contributions", __name__)
 
 @bp.route("/contributions")
 def get_contributions():
-    data = contributions_service()
-    return data
+    return contributions_service()
